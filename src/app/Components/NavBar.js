9c100d0e4baea8f59cdc9e9f5d3480a6e3af108b
baku67 <basile08@hotmail.fr>
2024-05-29 03:52:39 +0200

    Responsiv

--- src/app/Components/NavBar.js
+++ src/app/Components/NavBar.js
@@ -15,48 +15,49 @@ export function NavBar({isNavActive, toggleNav, currentActive}) {
             >
 
                 <div className="navBar-topLine">
+
+                    <Link href="/">
+                        <FontAwesomeIcon icon={faHouse} className="navBar-homeIcon" />
+                    </Link>
+
                     <span className="navBar-topLine-exitBtn" onClick={toggleNav}>
                         <FontAwesomeIcon icon={faXmark} />
                     </span>
                 </div>
 
+
+
                 <div className="navBar-list-wrapper">
+
                     <ul className="navBar-list-ul">
 
-                    <Link href="/">
-                        <li className={currentActive === '' ? "navBar-li-active" : ""}>
-                            <FontAwesomeIcon icon={faHouse} className="navBar-li-fa" />
-                            <span className="navBar-li-span">Accueil</span>
-                        </li>
-                    </Link>
+                        <Link href="/parcours">
+                            <li className={currentActive === 'parcours' ? "navBar-li-active" : ""}>
+                                <FontAwesomeIcon icon={faPersonRunning} className="navBar-li-fa" />
+                                <span className="navBar-li-span">Parcours</span>
+                            </li>
+                        </Link>
+                        
+                        <Link href="/projets">
+                            <li className={currentActive === 'projets' ? "navBar-li-active" : ""}>
+                                <FontAwesomeIcon icon={faLightbulb} className="navBar-li-fa" />
+                                <span className="navBar-li-span">Projets</span>
+                            </li>
+                        </Link>
 
-                    <Link href="/parcours">
-                        <li className={currentActive === 'parcours' ? "navBar-li-active" : ""}>
-                            <FontAwesomeIcon icon={faPersonRunning} className="navBar-li-fa" />
-                            <span className="navBar-li-span">Parcours</span>
-                        </li>
-                    </Link>
-                    
-                    <Link href="/projets">
-                        <li className={currentActive === 'projets' ? "navBar-li-active" : ""}>
-                            <FontAwesomeIcon icon={faLightbulb} className="navBar-li-fa" />
-                            <span className="navBar-li-span">Projets</span>
-                        </li>
-                    </Link>
-
-                    <Link href="/loisirs">
-                        <li className={currentActive === 'loisirs' ? "navBar-li-active" : ""}>
-                            <FontAwesomeIcon icon={faOtter} className="navBar-li-fa" />
-                            <span className="navBar-li-span">Loisirs</span>
-                        </li>
-                    </Link>
-                    
-                    <Link href="/contact">
-                        <li className={currentActive === 'contact' ? "navBar-li-active" : ""}>
-                            <FontAwesomeIcon icon={faEnvelopeOpenText} className="navBar-li-fa" />
-                            <span className="navBar-li-span">Contact</span>
-                        </li>
-                    </Link>
+                        <Link href="/loisirs">
+                            <li className={currentActive === 'loisirs' ? "navBar-li-active" : ""}>
+                                <FontAwesomeIcon icon={faOtter} className="navBar-li-fa" />
+                                <span className="navBar-li-span">Loisirs</span>
+                            </li>
+                        </Link>
+                        
+                        <Link href="/contact">
+                            <li className={currentActive === 'contact' ? "navBar-li-active" : ""}>
+                                <FontAwesomeIcon icon={faEnvelopeOpenText} className="navBar-li-fa" />
+                                <span className="navBar-li-span">Contact</span>
+                            </li>
+                        </Link>
 
                     </ul>
                 </div>
