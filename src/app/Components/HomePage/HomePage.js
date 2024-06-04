7c4d1fd700c9cc7cd2ba6184ee544980b49cf4d6
baku67 <basile08@hotmail.fr>
2024-06-04 20:06:35 +0200

    WIP player lofi HomePage

--- src/app/Components/HomePage/HomePage.js
+++ src/app/Components/HomePage/HomePage.js
@@ -15,7 +15,7 @@ import FontFaceObserver from 'fontfaceobserver';
 import Image from 'next/image'
 import Link from 'next/link';
 
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 
 
@@ -100,6 +100,9 @@ export function HomePage({isMobile}) {
 
 
     // Music smartphone WebGLRenderingContext;
+
+    const audioRef = useRef(null);
+    
     const [isMusicPlaying, setIsMusicPlaying] = useState(false);
     const [currentMusicTrackIndex, setCurrentMusicTrackIndex] = useState(0);
     
@@ -115,143 +118,169 @@ export function HomePage({isMobile}) {
             index: 1,
             title: "Coverless book",
             author: "AmbiantAUDIOVISION",
-            src: "/music/tracks/coverless-book-lofi-186307",
+            src: "/music/tracks/coverless-book-lofi-186307.mp3",
             credits: "Music by <a href='https://pixabay.com/users/ambientaudiovision-25188255/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=186307'>Amir Firouzfard</a> from <a href='https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=186307'>Pixabay</a>",
         },
         {
             index: 2,
             title: "LoFi Orchestra",
             author: "xethrocc",
-            src: "/music/tracks/lofi-orchestra-162306",
+            src: "/music/tracks/lofi-orchestra-162306.mp3",
             credits: "Music by <a href='https://pixabay.com/users/xethrocc-12734491/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=162306'>Patrick A.</a> from <a href='https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=162306'>Pixabay</a>",
         },
         {
             index: 3,
             title: "Wraps (174,gm)",
             author: "tobylane",
-            src: "/music/tracks/wraps-174gm-203739",
+            src: "/music/tracks/wraps-174gm-203739.mp3",
             credits: "Music by <a href='https://pixabay.com/users/tobylane-15168815/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=203739'>Toby Smith</a> from <a href='https://pixabay.com/music//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=203739'>Pixabay</a>",
         },
         {
             index: 4,
             title: "deep - lofi vibes",
             author: "xethrocc",
-            src: "/music/tracks/deep-lofi-vibes-205062",
+            src: "/music/tracks/deep-lofi-vibes-205062.mp3",
             credits: "Music by <a href='https://pixabay.com/users/xethrocc-12734491/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=205062'>Patrick A.</a> from <a href='https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=205062'>Pixabay</a>",
         },
         {
             index: 5,
             title: "Lofi Relax - Travel",
             author: "Lofium",
-            src: "/music/tracks/lofi-relax-travel-by-lofium-123560",
+            src: "/music/tracks/lofi-relax-travel-by-lofium-123560.mp3",
             credits: "Music by <a href='https://pixabay.com/users/lofium-30660321/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=123560'>Lofium</a> from <a href='https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=123560'>Pixabay</a>",
         },
         {
             index: 6,
             title: "Lofi Girl Dreams",
             author: "chillmore",
-            src: "/music/tracks/lofi-girl-dreams-113883",
+            src: "/music/tracks/lofi-girl-dreams-113883.mp3",
             credits: "Music by <a href='https://pixabay.com/users/chillmore-25283030/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=113883'>Chillmore</a> from <a href='https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=113883'>Pixabay</a>",
         },
         {
             index: 7,
             title: "Lofi Study",
             author: "FASSounds",
-            src: "/music/tracks/lofi-study-112191",
+            src: "/music/tracks/lofi-study-112191.mp3",
             credits: "Music by <a href='https://pixabay.com/users/fassounds-3433550/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=112191'>FASSounds</a> from <a href='https://pixabay.com/music//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=112191'>Pixabay</a>",
         },
         {
             index: 8,
             title: "Good Night",
             author: "FASSounds",
-            src: "/music/tracks/good-night-160166",
+            src: "/music/tracks/good-night-160166.mp3",
             credits: "Music by <a href='https://pixabay.com/users/fassounds-3433550/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=160166'>FASSounds</a> from <a href='https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=160166'>Pixabay</a>",
         },
         {
             index: 9,
             title: "Lofi Vibes",
             author: "chillmore",
-            src: "/music/tracks/lofi-vibes-113884",
+            src: "/music/tracks/lofi-vibes-113884.mp3",
             credits: "Music by <a href='https://pixabay.com/users/chillmore-25283030/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=113884'>Chillmore</a> from <a href='https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=113884'>Pixabay</a>",
         },
         {
             index: 10,
             title: "Avenue",
             author: "xethrocc",
-            src: "/music/tracks/avenue-lofi-154343",
+            src: "/music/tracks/avenue-lofi-154343.mp3",
             credits: "Music by <a href='https://pixabay.com/users/xethrocc-12734491/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=154343'>Patrick A.</a> from <a href='https://pixabay.com/music//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=154343'>Pixabay</a>",
         },
         {
             index: 11,
             title: "Spirit Blossom",
             author: "RomanBelov",
-            src: "/music/tracks/spirit-blossom-15285",
+            src: "/music/tracks/spirit-blossom-15285.mp3",
             credits: "Music by <a href='https://pixabay.com/users/romanbelov-25347333/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=15285'>Roman Belov</a> from <a href='https://pixabay.com/music//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=15285'>Pixabay</a>",
         },
         {
             index: 12,
             title: "drive - lofi vibes",
             author: "xethrocc",
-            src: "/music/tracks/drive-lofi-vibes-205061",
+            src: "/music/tracks/drive-lofi-vibes-205061.mp3",
             credits: "Music by <a href='https://pixabay.com/users/xethrocc-12734491/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=205061'>Patrick A.</a> from <a href='https://pixabay.com/music//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=205061'>Pixabay</a>",
         },
         {
             index: 13,
             title: "Lofi Chill",
             author: "FASSounds",
-            src: "/music/tracks/lofi-chill-140858",
+            src: "/music/tracks/lofi-chill-140858.mp3",
             credits: "Music by <a href='https://pixabay.com/users/fassounds-3433550/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=140858'>FASSounds</a> from <a href='https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=140858'>Pixabay</a>",
         },
         {
             index: 14,
             title: "Summer Sunset LoFi - slow mix",
             author: "xethrocc",
-            src: "/music/tracks/summer-sunset-lofi-slow-mix-154447",
+            src: "/music/tracks/summer-sunset-lofi-slow-mix-154447.mp3",
             credits: "Music by <a href='https://pixabay.com/users/xethrocc-12734491/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=154447'>Patrick A.</a> from <a href='https://pixabay.com/music//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=154447'>Pixabay</a>",
         },
         {
             index: 15,
             title: "Lofi Type Beat - Evening Stroll",
             author: "Nikoo1",
-            src: "/music/tracks/free-lofi-type-beat-evening-stroll-209514",
+            src: "/music/tracks/free-lofi-type-beat-evening-stroll-209514.mp3",
             credits: "Music by <a href='https://pixabay.com/users/xethrocc-12734491/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=154447'>Patrick A.</a> from <a href='https://pixabay.com/music//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=154447'>Pixabay</a>",
         },
         {
             index: 16,
             title: "Chill LoFi",
             author: "SoulProdMusic",
-            src: "/music/tracks/chill-lofi-160893",
+            src: "/music/tracks/chill-lofi-160893.mp3",
             credits: "Music by <a href='https://pixabay.com/users/soulprodmusic-30064790/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=160893'>Oleg Fedak</a> from <a href='https://pixabay.com/music//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=160893'>Pixabay</a>",
         },
         {
             index: 17,
             title: "Playa del Sol - latin lofi",
             author: "xethrocc",
-            src: "/music/tracks/playa-del-sol-latin-lofi-160149",
+            src: "/music/tracks/playa-del-sol-latin-lofi-160149.mp3",
             credits: "Music by <a href='https://pixabay.com/users/xethrocc-12734491/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=160149'>Patrick A.</a> from <a href='https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=160149'>Pixabay</a>",
         },
         {
             index: 18,
             title: "Lost Ambiant Lofi 60s",
             author: "Lesfm",
-            src: "/music/tracks/lost-ambient-lofi-60s-10821",
+            src: "/music/tracks/lost-ambient-lofi-60s-10821.mp3",
             credits: "Music by <a href='https://pixabay.com/users/lesfm-22579021/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=10821'>Oleksii Kaplunskyi</a> from <a href='https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=10821'>Pixabay</a>",
         },
         {
             index: 19,
             title: "Garden Vibes lofi",
             author: "xethrocc",
-            src: "/music/tracks/garden-vibes-lofi-160144",
+            src: "/music/tracks/garden-vibes-lofi-160144.mp3",
             credits: "Music by <a href='https://pixabay.com/users/xethrocc-12734491/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=160144'>Patrick A.</a> from <a href='https://pixabay.com/music//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=160144'>Pixabay</a>",
         },
     ]
 
     const handlePlayPauseClick = () => {
-
-        setIsMusicPlaying(!isMusicPlaying);
-
+        // by checking if (audioRef.current) before calling play() or pause(), you're ensuring that these methods are only called when the <audio> element is available and has been successfully mounted in the DOM. This helps prevent runtime errors and ensures that your code behaves as expected.
+        if (audioRef.current) {
+            if (isMusicPlaying) {
+                audioRef.current.pause(); 
+            } else {
+                audioRef.current.play(); 
+            }
+            setIsMusicPlaying(!isMusicPlaying); // Toggle state
+        }
     }
 
+
+
+    useEffect(() => {
+        if (isMusicPlaying) {
+            audioRef.current.currentTime = 0;
+            audioRef.current.play();
+        }
+    }, [currentMusicTrackIndex]); // Exécuter cet effet chaque fois que currentMusicTrackIndex change
+
+    // Prev track
+    const handlePrevMusicClick = () => {
+        const prevIndex = currentMusicTrackIndex === 0 ? musicTracks.length - 1 : currentMusicTrackIndex - 1;
+        setCurrentMusicTrackIndex(prevIndex);
+    };
+
+    // Next track
+    const handleNextMusicClick = () => {
+        const nextIndex = currentMusicTrackIndex === musicTracks.length - 1 ? 0 : currentMusicTrackIndex + 1;
+        setCurrentMusicTrackIndex(nextIndex);
+    };
 
 
 
@@ -340,6 +369,7 @@ export function HomePage({isMobile}) {
                                 transform: isPostItHovered ? "translate(-2px, 5px) rotate(1deg)" : "translate(0px, 0px)",
                                 filter: isPostItHovered ? "drop-shadow(6px 5px 5px black)" : "drop-shadow(6px 5px 5px black)",
                             }}
+                            alt={"post-it"}
                         />
 
                         <div className="post-it-textDiv"
@@ -368,8 +398,13 @@ export function HomePage({isMobile}) {
                     style={{width:"fit-content"}}
                 >
 
-                    {/* <Image src="/plant-2.png" width={450} height={450} /> */}
-                    <Image src="/plant-1-min.png" width={isMobile ? 450 : 520} height={isMobile ? 450 : 520} className={`plantAccueil ${isFadingOut ? "fadeOut" : ""}`} />
+                    <Image 
+                        src="/plant-1-min.png" 
+                        width={isMobile ? 450 : 520} 
+                        height={isMobile ? 450 : 520} 
+                        className={`plantAccueil ${isFadingOut ? "fadeOut" : ""}`} 
+                        alt={"decoration plant"} 
+                    />
 
                     <div className={`bocal-exterieur ${isFadingOut ? "fadeOut" : ""}`}>
                         <div className="bocal-ouverture"></div>
@@ -387,6 +422,7 @@ export function HomePage({isMobile}) {
                         height={320} 
                         style={{transform: "rotate(182deg)", filter:"hue-rotate(334deg)"}}
                         className={`fishBowl-img ${isFadingOut ? "fadeOut" : ""}`}
+                        alt={"fish bowl decoration"}
                     />
                 </div>
 
@@ -494,7 +530,7 @@ export function HomePage({isMobile}) {
                                     <div className="youtubeWidget-div">
 
                                         {/* <audio/> HTML elem caché */}
-                                        <audio src={musicTracks[currentMusicTrackIndex].src} />
+                                        <audio ref={audioRef} src={musicTracks[currentMusicTrackIndex].src} />
 
                                         {/* Webp lofiGirl */}
                                         <div className="youtubeWidget-top">
@@ -505,12 +541,14 @@ export function HomePage({isMobile}) {
                                                     height={55}
                                                     className="youtubeWidget-img"
                                                     unoptimized
+                                                    alt={"music player thumbnail lofiGirl"}
                                                 />
                                             </div>
 
                                             {/* infos (titre deroulant, logo widget) TODO: scroll overflow*/}
                                             <div className="youtubeWidget-titleWrapper">
-                                                <span className="youtubeWidget-title">azayaka - Sunshine</span>
+                                                <span className="youtubeWidget-title">{musicTracks[currentMusicTrackIndex].title}</span>
+                                                <span className="youtubeWidget-author">{musicTracks[currentMusicTrackIndex].author}</span>
                                             </div>
 
                                         </div>
@@ -519,7 +557,9 @@ export function HomePage({isMobile}) {
                                         <div className="youtubeWidget-bottom">
 
                                             <div className="youtubeWidget-bottomControls">
-                                                <span><FontAwesomeIcon icon={faBackward} className="youtubeWidget-bottomControls-btn" /></span>
+                                                <span>
+                                                    <FontAwesomeIcon icon={faBackward} className="youtubeWidget-bottomControls-btn" onClick={handlePrevMusicClick} />
+                                                </span>
                                                 <span>
                                                     <FontAwesomeIcon 
                                                         icon={isMusicPlaying ? faPause : faPlay} 
@@ -528,7 +568,9 @@ export function HomePage({isMobile}) {
                                                         style={{width: "17px"}} /* Parce que taille differente play/pause Icone */
                                                     />
                                                 </span>
-                                                <span><FontAwesomeIcon icon={faForward} className="youtubeWidget-bottomControls-btn" /></span>
+                                                <span>
+                                                    <FontAwesomeIcon icon={faForward} className="youtubeWidget-bottomControls-btn" onClick={handleNextMusicClick} />
+                                                </span>
                                             </div>
 
                                             {/* Barre d'avancement */}
